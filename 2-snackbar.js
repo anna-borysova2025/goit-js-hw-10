a71f896cdbf865b2183e0d519479e5f00db71acd
anna-borysova2025 <anna@pavlyuk.org>
2025-11-17 16:55:24 +0000

    Deploying to gh-pages from @ anna-borysova2025/goit-js-hw-10@debd29a57b9244c57a0758f7e34c70781705f38b 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i}from"./assets/vendor-BbbuE1sJ.js";const n=document.querySelector(".form");n.addEventListener("submit",s=>{s.preventDefault();const o=s.target,t=+o.elements.delay.value,m=o.elements.state.value;new Promise((e,r)=>{setTimeout(()=>{m==="fulfilled"?e(t):r(t)},t)}).then(e=>{i.success({message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({message:`Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
